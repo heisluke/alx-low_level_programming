b626d9a5c60df93435b116a9fa29ff82423c9b38
heisluke <lukebamtefa@gmail.com>
2022-04-06 09:59:06 -0700

    main h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,21 +1,11 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-#include <stdio.h>
-#include <stdlib.h>
-
-char *_strdup(char *str);
-
-char **strtow(char *str);
-
-char *argstostr(int ac, char **av);
-
-char *str_concat(char *s1, char *s2);
-
-void free_grid(int **grid, int height);
-
-int **alloc_grid(int width, int height);
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif
